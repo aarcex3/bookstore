264999a2626772b74f386d5b898ff4e8126af78b
Agustin Arce <agustinarceprincich@gmail.com>
2024-05-14 18:30:02 -0400

    Delete Book Endpoint implemented

--- routes/book.go
+++ routes/book.go
@@ -35,7 +35,13 @@ func UpdateBook(c *gin.Context) {
 
 }
 func DeleteBook(c *gin.Context) {
-	c.JSON(200, gin.H{
-		"message": "deleteBook",
-	})
+	var book models.Book
+	if err := models.DB.Where("id = ?", c.Param("id")).First(&book).Error; err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Record not found!"})
+		return
+	}
+
+	models.DB.Delete(&book)
+
+	c.JSON(http.StatusOK, gin.H{"data": true})
 }
